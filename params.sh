2b48cf1b6db8c5f3b91ae1eea8f06f3de400f97e
Josef Ringgenberg <jringgenberg@zymergen.com>
2020-05-07 15:09:54 -0700

    update params

--- params.sh
+++ params.sh
@@ -1,7 +1,7 @@
-export DOCKER_TAG=Release_2020_03_1
+export DOCKER_TAG=Release_2020_03_1__PG12
 export GIT_REPO=https://github.com/rdkit/rdkit.git
 export GIT_BRANCH=Release_2020_03
-export GIT_TAG=$DOCKER_TAG
+export GIT_TAG=Release_2020_03_1
 
 export BASE=informaticsmatters
 echo "DOCKER_TAG=$DOCKER_TAG GIT_BRANCH=$GIT_BRANCH GIT_TAG=$GIT_TAG"
